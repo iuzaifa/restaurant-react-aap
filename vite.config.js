1e296cc354cc96ee4ab5f10daa961a6820851774
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-08-08 02:53:59 +0530

    update-- react

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base : process.env.VITE_BASE_PATH || '/restaurant-react-aap',
+// })
+
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base : process.env.VITE_BASE_PATH || '/restaurant-react-aap',
-})
+  base: '/', // This tells Vite to build asset URLs for root
+});
